6316c8a50fc7940d1ca293f9a8813e5e7d783bb6
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-11-20 21:52:46 +0800

    Added data annotation validation

--- Backend/DTO/AuthDTO/RegisterDTO.cs
+++ Backend/DTO/AuthDTO/RegisterDTO.cs
@@ -1,12 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.DTO.AuthDTO;
 
 public class RegisterDTO
 {
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress]
     public string email { get; set; } = null!;
+    [Required(ErrorMessage = "Username is required")]
     public string username { get; set; } = null!;
+    [Required(ErrorMessage = "Password is required")]
+    [StringLength(100, ErrorMessage = "Password must be atleast 8 characters long.", MinimumLength = 8)]
     public string password { get; set; } = null!;
+    [Required(ErrorMessage = "Confirm Password is required")]
     public string confirmPassword { get; set; } = null!;
+    [Required(ErrorMessage = "First Name is required")]
     public string firstName { get; set; } = null!;
+    [Required(ErrorMessage = "Middle Name is required")]
     public string middleName { get; set; } = null!;
+    [Required(ErrorMessage = "Sur Name is required")]
     public string surName { get; set; } = null!;
 }
