b249303e9008dabb576f3f73c4fb1fe5381ea8c2
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-11-20 21:53:34 +0800

    Added try catch and proper api response

--- Backend/Services/UserServices.cs
+++ Backend/Services/UserServices.cs
@@ -11,9 +11,51 @@ public class UserServices
         _repository = repository;
     }
 
-    public async Task<string> registerUser(RegisterDTO user)
+    public async Task<APIResponseDTO<LoginDTO>> registerUser(RegisterDTO user)
     {
-        var response = await _repository.AddUser(user);
-        return response;
+        try
+        {
+            if (user.confirmPassword != user.password)
+            {
+                return new APIResponseDTO<LoginDTO>
+                {
+                    success = false,
+                    statusCode = 404,
+                    message = "Incorrect Password",
+                    Errors = new List<string> {"Password mismatched."}
+                };
+            }
+            var checkUser = await _repository.UserExists(user.username, user.email);
+            if (checkUser)
+            {
+                return new APIResponseDTO<LoginDTO>
+                {
+                    success = false,
+                    statusCode = 400,
+                    message = "User already exists.",
+                    Errors = new List<string> { "Username or email is already taken." }
+                };
+            }
+            var response = await _repository.AddUser(user);
+            return new APIResponseDTO<LoginDTO>
+            {
+                success = true,
+                statusCode = 200,
+                message = $"Welcome {user.username}. Account Created.",
+                data = null
+            };
+            
+        }
+        catch (Exception e)
+        {
+            return new APIResponseDTO<LoginDTO>
+            {
+                success = false,
+                statusCode = 500,
+                message = e.Message,
+                data = null
+            };
+        }
+        
     }
 }
