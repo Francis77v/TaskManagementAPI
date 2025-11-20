e773726dc4b34d14255ea29d261ffc174bc99731
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-11-20 21:00:44 +0800

    add not null on properties.

--- Backend/DTO/AuthDTO/RegisterDTO.cs
+++ Backend/DTO/AuthDTO/RegisterDTO.cs
@@ -2,11 +2,11 @@ namespace Backend.DTO.AuthDTO;
 
 public class RegisterDTO
 {
-    public string email { get; set; }
-    public string username { get; set; }
-    public string password { get; set; }
-    public string confirmPassword { get; set; }
-    public string firstName { get; set; }
-    public string middleName { get; set; }
-    public string surName { get; set; }
+    public string email { get; set; } = null!;
+    public string username { get; set; } = null!;
+    public string password { get; set; } = null!;
+    public string confirmPassword { get; set; } = null!;
+    public string firstName { get; set; } = null!;
+    public string middleName { get; set; } = null!;
+    public string surName { get; set; } = null!;
 }
